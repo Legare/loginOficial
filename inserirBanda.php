e2dd38ada16295c663a6e10851c0b17968f034a8
ANDREZA_003081 <ANDREZA_003081@LAB4M02.itbeng.inf>
2014-07-31 14:51:05 -0300

    Criptografia

--- inserirBanda.php
+++ inserirBanda.php
@@ -1,11 +1,13 @@
 <?php
 echo '<meta charset=UTF-8>';
+include_once 'classes/Bcrypt.class';
 include_once 'conexao/conecta.inc';
 $banda = $_POST['banda'];
 $senha = $_POST['senhabanda'];
 $email =  $_POST['senhabanda'];
 $genero =  $_POST['genero'];
 $tipoUsuario= 'BAN';
+$senha = Bcrypt::hash($senha);
 
 $query = "INSERT INTO banda (EMAIL_BANDA,SENHA_BANDA,GENERO_BANDA,NOME_BANDA,TIPO_BANDA)";
 $query.= " VALUES( '$email','$senha','$genero','$banda','$tipoUsuario')";
